74d28515edcbef37acdf02a6005ab86d628f3e2c
faustinopsy <rodrigohipnose@gmail.com>
2025-04-10 10:45:43 -0300

    correção cep do cache

--- js/utils/api.js
+++ js/utils/api.js
@@ -1,7 +1,19 @@
+const cache = {}
 export const getCEP = async (cep) => {
+    console.log(cache[cep])
+    console.time("cache");
+    if (cache[cep]) {
+        console.log(`cache: Dados carregados do cache: ${JSON.stringify(cache[cep])}`);
+        console.timeLog("cache");
+        return cache[cep];
+    }
+    console.time("api");
   try {
     const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     const data = await response.json()
+    console.log(`api: Dados carregados da API: ${JSON.stringify(data)}`);
+    console.timeLog("api");
+    cache[cep] = data;
     return data
   } catch (error) {
     console.error('Erro ao buscar cep:', error);
